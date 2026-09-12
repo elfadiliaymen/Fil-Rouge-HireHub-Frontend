87b449af344189a6086c4a4bf7912e8bb4e10348
aymen EL FADILI <aymenelfadili0@gmail.com>
2026-09-12 20:43:08 +0100

    feat: base app entry, global styles and api client

---
+++ src/api/api.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8090/api",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 400:
+          alert("Requête invalide.");
+          break;
+        case 401:
+          localStorage.removeItem("token");
+          alert("Session expirée. Veuillez vous reconnecter.");
+          window.location.href = "/login";
+          break;
+        case 403:
+          alert("Accès interdit.");
+          break;
+        case 404:
+          alert("Ressource introuvable.");
+          break;
+        case 500:
+          alert("Erreur interne du serveur.");
+          break;
+        default:
+          alert("Une erreur est survenue.");
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
